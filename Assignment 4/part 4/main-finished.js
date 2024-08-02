c4bba421b9abc80de2a0df82181968d43b7a112c
Sudan99 <sudan.parajuli@dcmail.ca>
2024-08-02 16:02:15 -0400

    ball draw, function, update and collision was edited

--- Assignment 4/part 4/main-finished.js
+++ Assignment 4/part 4/main-finished.js
@@ -9,17 +9,63 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+
 // function to generate random number
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+function Shape(x, y, velX, velY, exist) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.exist = exist;
 }
+function Ball(x, y, velX, velY, color, size, exist) {
+  Shape.call(this, x, y, velX, velY, exist);
+  this.color = color;
+  this.size = size;
+}
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+Ball.prototype.update = function () {
+  this.x += this.velX;
+  this.y += this.velY;
+  if (this.size + this.x >= width) {
+    this.velX = -this.velX;
+  } else if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+  } else if (this.size + this.y >= height) {
+    this.velY = -this.velY;
+  } else if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+  }
+};
+Ball.prototype.collision = function () {
+  for (let k = 0; k < balls.length; k++) {
+    if (!(this === balls[k])) {
+      const dx = this.x - balls[k].x;
+      const dy = this.y - balls[k].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[k].size) {
+        balls[k].color = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(
+          0,
+          255
+        )})`;
+      }
+    }
+  }
+};
+
+
+
 
 class Ball {
   constructor(x, y, velX, velY, color, size) {

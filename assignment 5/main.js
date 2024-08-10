6b2bb0fc9113cd59e774fc11804c21b9e96ebbe7
Sudan99 <sudan.parajuli@dcmail.ca>
2024-08-10 02:13:56 -0400

    edited in function submit comment, name value and comment value was added

--- assignment 5/main.js
+++ assignment 5/main.js
@@ -30,18 +30,12 @@ form.onsubmit = function(e) {
 
 function submitComment() {
   const listItem = document.createElement('li');
+  const nameLabel = document.createElement('span');
+  const commentLabel = document.createElement('span');
   const namePara = document.createElement('p');
   const commentPara = document.createElement('p');
   const nameValue = nameField.value;
   const commentValue = commentField.value;
 
-  namePara.textContent = nameValue;
-  commentPara.textContent = commentValue;
 
-  list.appendChild(listItem);
-  listItem.appendChild(namePara);
-  listItem.appendChild(commentPara);
 
-  nameField.value = '';
-  commentField.value = '';
-}

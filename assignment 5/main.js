cb275015d27e9cd1de125d3d8674f80954411a53
Sudan99 <sudan.parajuli@dcmail.ca>
2024-08-10 02:17:36 -0400

    edited in control transcript display and modified in the code

--- assignment 5/main.js
+++ assignment 5/main.js
@@ -64,5 +64,23 @@ function submitComment() {
   commentField.value = '';
 }
 
+// Control transcript display
+console.log("here")
+const transcript = document.querySelector('.transcript');
+const transcriptBtn = document.querySelector('.transcript-container button');
+
+transcriptBtn.addEventListener("click", toggleTranscript);
+
+function toggleTranscript() {
+  if(transcriptBtn.textContent === 'Show transcript') {
+    transcript.style.height = '150px';
+    transcriptBtn.textContent = 'Hide transcript';
+  } else {
+    transcript.style.height = '0';
+    transcriptBtn.textContent = 'Show transcript';
+  }
+};
+
+
 
 
